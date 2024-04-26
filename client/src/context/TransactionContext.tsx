09d1644e412682172bfb36c28b266f23c7a11ad5
Harsh Dubey <80833187+Harsh-DubeySJA@users.noreply.github.com>
2024-04-26 13:35:11 +0530

    Add files via upload

--- client/src/context/TransactionContext.tsx
+++ client/src/context/TransactionContext.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
 import { contractABI, contractAddress } from "../utils/constants";
+import { IoConstructOutline } from "react-icons/io5";
 
 export const TransactionContext = React.createContext();
 
@@ -12,15 +13,21 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log({
-        provider,
-        signer,
-        transactionContract
-    })
+    return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
-    const [connectedAccount, setConnectedAccount] = useState("");
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: ""});
+    const [isloading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
+
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({
+            ...prevState, [name]: e.target.value
+        }));
+    }
+
     const checkWalletIsConnected = async () => {
         if (!ethereum) return alert("Please install metamask");
 
@@ -38,6 +45,8 @@ export const TransactionProvider = ({ children }) => {
                 setCurrentAccount(accounts[0]);
 
                 // getAllTransactions()
+            } else {
+                IoConstructOutline.log("No accounts found")
             }
         } catch (error) {
             console.log(error);
@@ -46,12 +55,48 @@ export const TransactionProvider = ({ children }) => {
         }
     }
 
+    const sendTransaction = async () => {
+        try {
+            if (!ethereum) return alert("Please install Metamask");
+
+            const { addressTo, amount, keyword, message } = formData;
+            const transactionContract = getEthereumContract();
+            const parsedAmount = ethers.utils.parseEther(amount);
+            
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: "0x5208", // 21000 GWEI
+                    value: parsedAmount._hex, // 0.00001
+                }]
+            });
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`)
+
+            const transactionCount = await transactionContract.getTransactionCount();
+
+            setTransactionCount(transactionCount);
+        } catch(error) {
+            console.log(error);
+
+            throw new Error("No Ethereum object found");
+        }
+    }
+
     useEffect(() => {
         checkWalletIsConnected();
     }, [])
 
     return (
-        <TransactionContext.Provider value={{ connectWallet }}>
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, sendTransaction, handleChange }}>
             {children}
         </TransactionContext.Provider>
     )
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractABI = abi.abi;
 export const contractAddress = "0xaac1db467455a634dc8849e87a64a78d7a85860759a43d1f5465faf45d5eac2b";
+export const contractABI = abi.abi;
