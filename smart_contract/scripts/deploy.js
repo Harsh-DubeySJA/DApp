901c69ad98a686df9697a023ef624827c21d4fb9
Harsh Dubey <80833187+Harsh-DubeySJA@users.noreply.github.com>
2024-04-26 13:36:16 +0530

    Add files via upload

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,10 +1,10 @@
 const main = async () => {
-    const Transactions = await hre.ethers.getContractFactory("Transactions");
-    const transactions = await Transactions.deploy();
+    const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+    const transactionsContract = await transactionsFactory.deploy();
 
-    await transactions.waitForDeployment();
+    await transactionsContract.deployed();
 
-    console.log("Transactions deployed to:", transactions.address);
+    console.log("Transactions deployed to:", transactionsContract.address);
 }
 
 const runMain = async () => {
